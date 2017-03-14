335b1bfaacae3dd8d2ea3950aaf447c42ede75bf
mingxiang <mx3434@hotmail.com>
2017-03-14 15:30:21 +0800

    Add todos reducers and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+import uuid from 'node-uuid';
+import moment from 'moment';
+
 export let searchTextReducer = (state='', action) => {
   switch(action.type) {
     case 'SET_SEARCH_TEXT':
@@ -12,6 +15,43 @@ export let showCompletedReducer = (state=false, action) => {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state;
     default:
+      return state;
+  }
+}
+
+export let todosReducer = (state=[], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text : action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ];
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+      /*  if(todo.id === action.id){
+          return {
+            ...todo,
+            completed : !todo.completed,
+            completedAt : (todo.completed) ? moment().unix() : undefined
+          }
+        }
+*/
+        if(todo.id === action.id) {
+          return {
+            ...todo,
+            completed : !todo.completed,
+            completedAt : (todo.completed) ? undefined : moment().unix()
+          }
+        }
+        return todo;
+      })
+    default :
       return state;
   }
 }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,6 +1,6 @@
 import expect from 'expect';
 import df from 'deep-freeze-strict';
-import {searchTextReducer, showCompletedReducer} from 'reducers';
+import {searchTextReducer, showCompletedReducer, todosReducer} from 'reducers';
 
 describe('Reducers', () => {
   describe('searchTextReducer', () => {
@@ -23,4 +23,35 @@ describe('Reducers', () => {
         expect(res).toEqual(false);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      let action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      }, res = todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      let action = {
+        type: 'TOGGLE_TODO',
+        id : 123
+      }, todos = [
+        {
+          id: 123,
+          text: 'testing todo',
+          completed: true,
+          createdAt: 123,
+          completedAt: 123
+        }
+      ], res = todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+
+    })
+  })
 });
